efbdc8b8a5eaee37bf836fd152624614188fc567
Rune Madsen <rune@runemadsen.com>
2014-01-20 13:58:22 -0500

    tweaking headers and so

--- wp-content/themes/starkers/page.php
+++ wp-content/themes/starkers/page.php
@@ -26,13 +26,9 @@
     )); ?>
   </div>
   <div class="col-md-9" id="right">
-
     <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-    <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
-    <?php comments_template( '', true ); ?>
+      <?php the_content(); ?>
     <?php endwhile; ?>
-
   </div>
 </div>
 
--- wp-content/themes/starkers/parts/shared/header.php
+++ wp-content/themes/starkers/parts/shared/header.php
@@ -1,5 +1,9 @@
 <div class="row" id="header">
-  <div class="col-md-3"><a id="logo" href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></div>
+  <div class="col-md-3">
+    <a id="logo" href="<?php echo home_url(); ?>">
+      <img name=image src="<?php bloginfo('template_directory'); ?>/images/logo.png" />
+    </a>
+  </div>
   <div class="col-md-9">
     <?php wp_nav_menu( array(
       'theme_location' => 'main-menu',
@@ -7,3 +11,4 @@
     )); ?>
   </div>
 </div>
+<hr id="header-line" />
