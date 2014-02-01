a98e8ccf0905b69a0d4397c2727750f1ec4fa118
Rune Madsen <rune@runemadsen.com>
2014-02-01 15:38:33 -0500

    adding a bunch of things

--- wp-content/themes/starkers/page.php
+++ wp-content/themes/starkers/page.php
@@ -18,17 +18,22 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <div class="row" id="main">
-  <div class="col-md-3" id="left">
-    <?php wp_nav_menu( array(
-      'theme_location' => 'sub-menu',
-      'depth' => 2,
-      'walker'=> new Selective_Walker() 
-    )); ?>
-  </div>
-  <div class="col-md-9" id="right">
+  <div class="col-md-7" id="content">
     <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
       <?php the_content(); ?>
     <?php endwhile; ?>
+  </div>
+  <div class="col-md-5" id="attachments">
+    <?php $attachments = new Attachments( 'attachments' ); /* pass the instance name */ ?>
+    <?php if( $attachments->exist() ) : ?>
+        <?php while( $attachments->get() ) : ?>
+          <figure class="attachment">
+            <img src="<?php echo $attachments->url(); ?>" class="img-responsive" />
+            <figcaption><?php echo $attachments->field( 'caption' ); ?></figcaption>
+          </figure>
+        <?php endwhile; ?>
+      </ul>
+    <?php endif; ?>
   </div>
 </div>
 
--- wp-content/themes/starkers/parts/shared/header.php
+++ wp-content/themes/starkers/parts/shared/header.php
@@ -12,3 +12,12 @@
   </div>
 </div>
 <hr id="header-line" />
+<div class="row" id="sub-header">
+  <div class="col-md-12">
+  <?php wp_nav_menu( array(
+      'theme_location' => 'sub-menu',
+      'depth' => 2,
+      'walker'=> new Selective_Walker() 
+    )); ?>
+  </div>
+</div>
